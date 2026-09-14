27f8522c45f85bd83f3ebff6514fc6d1cee5ba73
Compilandog <compilandooconhecimento@gmail.com>
2026-09-14 17:49:20 -0300

    update 18: align persisted popup color with corrected moss

--- src/utils/portalAppearanceLinks.ts
+++ src/utils/portalAppearanceLinks.ts
@@ -5,8 +5,8 @@ export const TABLE_LAYOUTS_EVENT = 'global_table_layouts_changed';
 export const GLOBAL_POPUP_STYLE_KEY = 'portal_global_popup_style_v1';
 export const GLOBAL_POPUP_STYLE_EVENT = 'portal_global_popup_style_changed';
 
-const POPUP_MOSS = '#69786d';
-const LEGACY_POPUP_GREENS = new Set(['#005830', '#435649', '#344125']);
+const POPUP_MOSS = '#337959';
+const LEGACY_POPUP_GREENS = new Set(['#005830', '#435649', '#344125', '#69786d']);
 
 export interface GlobalPopupStyle {
   surfaceBgColor: string;
