54acaab069ea0a204b65c43667c585912d2380e9
Compilandog <compilandooconhecimento@gmail.com>
2026-09-14 15:54:07 -0300

    style: ativa sistema visual autoritativo

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import './portal-overrides.css';
+import './portal-design-system.css';
 
 import { loadGlobalPopupStyle, saveGlobalPopupStyle } from './utils/portalAppearanceLinks';
 
