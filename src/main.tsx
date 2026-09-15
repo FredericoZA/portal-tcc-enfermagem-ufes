a20a341fb6fbfd574ded962890d3a2ea8131dd80
Compilandog <compilandooconhecimento@gmail.com>
2026-09-15 13:56:51 -0300

    update 26: carregar camada visual final

--- src/main.tsx
+++ src/main.tsx
@@ -12,6 +12,7 @@ import './portal-update-22.css';
 import './portal-update-23.css';
 import './portal-update-24.css';
 import './portal-update-25.css';
+import './portal-update-26.css';
 
 import { loadGlobalPopupStyle, saveGlobalPopupStyle } from './utils/portalAppearanceLinks';
 
