0ecb1d173964d3bc280d8d69b6789c95d35631c1
Frederico <compilandooconhecimento@gmail.com>
2026-09-20 21:22:53 -0300

    ui: ativar camada consolidada de manutenção

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,7 @@ import App from './App.tsx';
 import { PortalUiEnhancer } from './components/PortalUiEnhancer';
 import { PortalSpreadsheetEnhancer } from './components/PortalSpreadsheetEnhancer';
 import { PortalVersion1040Enhancer } from './components/PortalVersion1040Enhancer';
-import { PortalSpreadsheetRefinementEnhancer } from './components/PortalSpreadsheetRefinementEnhancer';
-import { PortalInteractionRefinementEnhancer } from './components/PortalInteractionRefinementEnhancer';
+import { PortalMaintenanceEnhancer } from './components/PortalMaintenanceEnhancer';
 import './index.css';
 import './portal-overrides.css';
 import './portal-design-system.css';
@@ -27,8 +26,7 @@ import './portal-update-40.css';
 import './portal-update-42.css';
 import './portal-update-43.css';
 import './portal-version-1040.css';
-import './portal-spreadsheet-refinement.css';
-import './portal-interaction-refinement.css';
+import './portal-maintenance.css';
 import { loadGlobalPopupStyle, saveGlobalPopupStyle } from './utils/portalAppearanceLinks';
 import { installAuthRequestResilience } from './utils/authRequestResilience';
 
@@ -41,7 +39,6 @@ createRoot(document.getElementById('root')!).render(
     <PortalUiEnhancer />
     <PortalSpreadsheetEnhancer />
     <PortalVersion1040Enhancer />
-    <PortalSpreadsheetRefinementEnhancer />
-    <PortalInteractionRefinementEnhancer />
+    <PortalMaintenanceEnhancer />
   </StrictMode>,
 );
